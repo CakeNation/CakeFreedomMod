e1cd9dccad5305b46d7db230023e99885e6a8170
Kopi <ihessmc@gmail.com>
2015-12-13 12:28:06 -0800

    Not iRedemptFreedom :-1:, its CakeFreedom :+1:

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -1,6 +1,7 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,11 +14,25 @@ public class Command_admininfo extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
         
+     if (!TFM_AdminList.isSuperAdmin(sender_p)) {   
         sender_p.chat("I'm going to apply for administrator!");
         sender_p.chat("Hopefully, I'll get accepted by a Owner or a Developer!");
         TFM_Util.playerMsg(sender_p, "Apply Here:", ChatColor.BLUE);
-        TFM_Util.playerMsg(sender_p, "http://iredemptfreedom.boards.net/", ChatColor.GREEN);
-        return true;
+        TFM_Util.playerMsg(sender_p, "http://cakefreedom.boards.net/", ChatColor.GREEN);
+        TFM_Util.playerMsg(sender_p, "Objection means that you did something bad!", ChatColor.RED);
+        TFM_Util.playerMsg(sender_p, "Vouch means that an admin likes you,\nand your application!", ChatColor.GREEN);
+     }
         
+        if (TFM_AdminList.isSuperAdmin(sender_p)) {
+         
+            String url = "https://cakefreedom.boards.net";
+            playerMsg(ChatColor.RED + "You're an admin, you cannot apply");
+            playerMsg(ChatColor.GOLD + "But you can still view our forums\nURL: " + url);
+            playerMsg(ChatColor.BLUE + "You can also apply for a promotion!\n");
+         
+            
+        }
+        
+        return true;
     }
 }
